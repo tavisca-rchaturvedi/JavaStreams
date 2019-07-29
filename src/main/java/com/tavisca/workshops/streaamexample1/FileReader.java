2d7fa698fc92046a7ad08ebb20ec677912afbfe6
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-07-29 15:29:51 +0530

    FileInputStream with String Builder.

--- src/main/java/com/tavisca/workshops/streaamexample1/FileReader.java
+++ src/main/java/com/tavisca/workshops/streaamexample1/FileReader.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 public class FileReader {
     public String readFile(String fileName) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(fileName);
-        String contents = "";
+        StringBuilder contents = new StringBuilder();
+
         try (fileInputStream){
-            byte[] buffer = new byte[fileInputStream.available()];
-            fileInputStream.read(buffer);
-            contents = new String(buffer);
+            byte[] buffer = new byte[8];
+            while(fileInputStream.read(buffer) > 0){
+                System.out.println(new String(buffer));
+                contents.append(new String(buffer));
+                buffer = new byte[8];
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return contents;
+        return contents.toString();
     }
 }
