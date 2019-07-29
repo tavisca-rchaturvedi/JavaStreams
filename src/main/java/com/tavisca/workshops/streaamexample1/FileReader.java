a6babd906eecb78f95c2d2c46ca80c4b20bf9f50
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-07-29 15:31:17 +0530

    FileInputStream with BufferedInputStream.

--- src/main/java/com/tavisca/workshops/streaamexample1/FileReader.java
+++ src/main/java/com/tavisca/workshops/streaamexample1/FileReader.java
@@ -1,16 +1,19 @@
 package com.tavisca.workshops.streaamexample1;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FileReader {
     public String readFile(String fileName) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(fileName);
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
         StringBuilder contents = new StringBuilder();
 
-        try (fileInputStream){
+        try (bufferedInputStream){
             byte[] buffer = new byte[8];
-            while(fileInputStream.read(buffer) > 0){
+            while(bufferedInputStream.read(buffer) > 0){
                 System.out.println(new String(buffer));
                 contents.append(new String(buffer));
                 buffer = new byte[8];
